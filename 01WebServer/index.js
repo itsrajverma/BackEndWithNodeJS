0de046150e2d71a119181671131c4a14ee3d418e
Raj Verma <info.mrverma@gmail.com>
2019-07-10 21:31:28 +0530

    web server

---
+++ 01WebServer/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const host = "127.0.0.1";
+const port = 5000;
+
+http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World");
+}).listen(port, host, () => {
+    console.log(`Server Running On http://${host}:${port}`);
+});
